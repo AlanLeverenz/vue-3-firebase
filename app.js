bae0c34e5d9fabe4985c5f15f02e5b18d12e32a2
Alan Leverenz <awleverenz@aol.com>
2021-03-01 07:57:40 -0500

    data function objects to mount in template

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-console.log('hello, vue')
+// without a template object will get 'render' error
+const app = Vue.createApp({
+    // data, functions
+    data() {
+        return {
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    }
+})
+// mounts Vue in the html template DOM with id 'app'
+app.mount('#app')
 
