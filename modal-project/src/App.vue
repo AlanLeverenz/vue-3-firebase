e602ae4f266a02a69cd2a954c374aaeb4814d884
Alan Leverenz <awleverenz@aol.com>
2021-03-05 00:03:27 -0500

    template refs

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{ title }}</h1>
+    <input type="text" ref="name">
+    <button @click="handleClick">click me</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  // components: {
+  //   HelloWorld
+  // }
+  data() {
+    return {
+      title: 'My First Vue App :)'
+    }
+  },
+  // $refs references an element by ref
+  // classList is JS term that adds a class to an element <input>
+  // focus selects the element
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
   }
 }
 </script>
@@ -22,5 +40,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 { 
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
