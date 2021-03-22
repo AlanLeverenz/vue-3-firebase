f46a3cacef3139977f680a8fc2661ae19a0a8216
Alan Leverenz <awleverenz@aol.com>
2021-03-21 21:47:11 -0400

    programmatic navigation

--- ninja-jobs/src/App.vue
+++ ninja-jobs/src/App.vue
@@ -7,10 +7,35 @@
       <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
       <!-- data bind the path using the index.js route's object name -->
     </div>
+
+<!-- @click activates methods -->
+    <button @click="redirect">Redirect</button>
+    <button @click="back">Go back</button>
+    <button @click="forward">Go forward</button>
+
     <!-- router components are injected below in router-view -->
     <router-view/>
   </div>
 </template>
+
+<script>
+// use export default when a component object is inside a vue file.
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'Home' })
+    },
+    // go method steps through router history
+    back() {
+      this.$router.go(-1)
+    },
+    forward() {
+      this.$router.go(1)
+    }
+  }
+}
+</script>
+
 
 <style>
 #app {
@@ -31,11 +56,16 @@
   text-decoration: none;
   padding: 10px;
   border-radius: 4px;
-
 }
 /* built-in class for vue router-links */
 #nav a.router-link-exact-active {
   color: white;
   background: crimson;
+}
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>
