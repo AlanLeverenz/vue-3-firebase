ed212d3de74895fb1ee4d9c3f19fd7bd02f32006
Alan Leverenz <awleverenz@aol.com>
2021-03-24 18:12:00 -0400

    added db.json

--- project-planner/src/views/Home.vue
+++ project-planner/src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="projects.length">
+      <!-- binding key data -->
+      <div v-for="project in projects" :key="project.id"></div>
+      <p>{{ project.title }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
+  },
+  // data is returning project objects from db.json
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    // mounted() is a "hook" lifecycle
+    // these are asynchronous requests with promises returned (.then)
+    fetch('http://locahost:3000/projects')
+    .then(res => res.json())
+    .then(data => this.projects = data)
+    .catch(err => console.log(err.message))
   }
 }
 </script>
