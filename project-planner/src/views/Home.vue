e84c462a6aaa7d17fca20c6ca214596d876891a7
Alan Leverenz <awleverenz@aol.com>
2021-03-24 22:34:33 -0400

    fixed v-for div to render titles

--- project-planner/src/views/Home.vue
+++ project-planner/src/views/Home.vue
@@ -2,9 +2,12 @@
   <div class="home">
     <div v-if="projects.length">
       <!-- binding key data -->
-      <div v-for="project in projects" :key="project.id"></div>
-      <p>{{ project.title }}</p>
+      <!-- the object to render must be inside the div v-for element -->
+      <div v-for="project in projects" :key="project.id">
+        <p>{{ project.title }}</p>
+      </div>
     </div>
+    <div v-else>Houston, we've got a problem</div>
   </div>
 </template>
 
@@ -25,7 +28,7 @@ export default {
   mounted() {
     // mounted() is a "hook" lifecycle
     // these are asynchronous requests with promises returned (.then)
-    fetch('http://locahost:3000/projects')
+    fetch('http://localhost:3000/projects')
     .then(res => res.json())
     .then(data => this.projects = data)
     .catch(err => console.log(err.message))
