476e4947f8cb3a6a46a1bef74891ff9738b3d20d
Alan Leverenz <awleverenz@aol.com>
2021-03-28 23:52:53 -0400

    patch edit to project

--- project-planner/src/views/EditProject.vue
+++ project-planner/src/views/EditProject.vue
@@ -1,5 +1,5 @@
 <template>
-    <form>
+    <form @submit.prevent="handleSubmit">
         <label>Title:</label>
         <!-- v-model tracks the user's input -->
         <!-- this is 2-way data binding between form and data store -->
@@ -11,6 +11,12 @@
 </template>
 
 <script>
+// challenge
+//  - add a submit handler to update the project
+//  - make a fetch (PATCH) request to the uri to update the project
+// - redirect to the homepage when done
+
+
 // need data models to track what user types into the form
 // need to accept data objects as props. props = true in routes object.
 // use props 'id' in a fetch function to get elements from json-server db.
@@ -22,6 +28,23 @@ export default {
             details: '',
             // uri is for fetch request
             uri: 'http://localhost:3000/projects/' + this.id
+        }
+    },
+        methods: {
+        handleSubmit() {
+            // sends v-form field data to json-server
+            fetch(this.uri, {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify( { title: this.title, details: this.details })
+            // }).then(() => {
+            //     // updates Home page
+            //     this.$emit('update', this.project.id)
+            }).then(() => {
+                // resets to the Home page
+               this.$router.push('/') 
+               // callback function is console.log
+            }).catch((err) => console.log(err))
         }
     },
     // get data from json-server once the page loads
