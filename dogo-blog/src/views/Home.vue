99cf6a3e03af064112dfd0e8edc1ce8c7e27d4f8
Alan Leverenz <awleverenz@aol.com>
2021-04-01 19:09:06 -0400

    demoed setup in composition api

--- dogo-blog/src/views/Home.vue
+++ dogo-blog/src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-
+    home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
   </div>
 </template>
 
@@ -10,6 +12,31 @@
 
 export default {
   name: 'Home',
+  // setup() runs before any lifecycles
+  // can write javascript, set variables, etc. inside setup
+  // which can be used inside the template
+  // if used data() with objects, those are reactive
 
+  setup() {
+    console.log('setup')
+    // not reactive values
+    let name = 'mario'
+    let age = 30
+
+    const handleClick = () => {
+      console.log('you clicked me')
+    }
+
+    // values and functions must be returned to be usable in template
+
+    return { name, age, handleClick }
+
+  },
+  created() {
+    console.log('created')
+  },
+  mounted() {
+    console.log('mounted')
+  },
 }
 </script>
