75e16b145fdf626d72750e0943caa9bb3edc1c85
Alan Leverenz <awleverenz@aol.com>
2021-04-03 23:21:29 -0400

    making refs reactive

--- dogo-blog/src/views/Home.vue
+++ dogo-blog/src/views/Home.vue
@@ -3,6 +3,9 @@
     home
     <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
+    <button @click="age++">Add 1 to age</button>
+    <!-- don't need .value when addressing a ref in the template -->
+    <input type="text" v-model="name">
   </div>
 </template>
 
@@ -22,31 +25,37 @@ export default {
 
     // p is defined as a ref const, can assigned to template element
     // note: the value for p cannot be used until it is returned
-    const p = ref(null)
-    console.log(p, p.value)
+    // const p = ref(null)
+    // console.log(p, p.value)
 
     // not reactive values
-    let name = 'mario'
-    let age = 30
+    // whereas data() values are reactive and can be changed
+    // let name = 'mario'
+    // let age = 30
+
+    // when ref surrounds a value it becomes reactive
+    const name = ref('mario')
+    const age = ref(30)
 
     const handleClick = () => {
-      console.log(p, p.value)
-      // can assign a class to the ref object's class list
-      p.value.classList.add('test')
-      // this will replace the text of the p element
-      p.value.textContent = 'hello, ninjas'
+      name.value = 'luigi'
+      age.value = '35'
+      // console.log(p, p.value)
+      // // can assign a class to the ref object's class list
+      // p.value.classList.add('test')
+      // // this will replace the text of the p element
+      // p.value.textContent = 'hello, ninjas'
     }
 
     // values and functions must be returned to be usable in template
+    return { name, age, handleClick }
 
-    return { name, age, handleClick, p }
-
   },
-  created() {
-    console.log('created')
-  },
-  mounted() {
-    console.log('mounted')
-  },
+  // created() {
+  //   console.log('created')
+  // },
+  // mounted() {
+  //   console.log('mounted')
+  // },
 }
 </script>
