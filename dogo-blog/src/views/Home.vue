e20dd5da787b30225f22f9f8d8a855dd8c74b8f3
Alan Leverenz <awleverenz@aol.com>
2021-04-04 23:16:09 -0400

    refs and reactive

--- dogo-blog/src/views/Home.vue
+++ dogo-blog/src/views/Home.vue
@@ -4,6 +4,9 @@
     <h2>Refs</h2>
     <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
     <button @click="updateNinjaOne">Update ninja one</button>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo }}</p>
+    <button @click="updateNinjaTwo">Update ninja two</button>
   </div>
 </template>
 
@@ -15,12 +18,23 @@ export default {
   name: 'Home',
   setup() {
     const ninjaOne = ref({ name: 'mario', age: 30 })
+    const ninjaTwo = reactive({ name: 'luigi', age: 35})
 
+    const nameOne = ref('mario') // this is a reactive ref
+    const nameTwo = reactive('luigi') // not reactive because of a primitive value
+
+    // ref needs the value property
     const updateNinjaOne = () => {
       ninjaOne.value.age = 40
     }
 
-    return { ninjaOne, updateNinjaOne }
+    // reactive doesn't need the value property
+    // but it cannot update the value reactively (strange)
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 45
+    }
+
+    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo, nameTwo }
   }
 }
 </script>
