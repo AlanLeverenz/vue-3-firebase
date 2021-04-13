88149a6b87d9be9480d59f9e04563b508bc81ceb
Alan Leverenz <awleverenz@aol.com>
2021-04-12 21:53:01 -0400

    stylized the blog

--- dogo-blog/src/composables/getPost.js
+++ dogo-blog/src/composables/getPost.js
@@ -8,10 +8,10 @@ const getPost = (id) => {
 
     const load = async () => {
         try {
-            // simulate delay
-            await new Promise(resolve => {
-                setTimeout(resolve, 1000)
-            })
+            // // simulate delay
+            // await new Promise(resolve => {
+            //     setTimeout(resolve, 1000)
+            // })
 
             let data = await fetch('http://localhost:3000/posts/' + id)
             if (!data.ok) {
--- dogo-blog/src/composables/getPosts.js
+++ dogo-blog/src/composables/getPosts.js
@@ -7,10 +7,10 @@ const getPosts = () => {
 
     const load = async () => {
         try {
-            // simulate delay
-            await new Promise(resolve => {
-                setTimeout(resolve, 1000)
-            })
+            // // simulate delay
+            // await new Promise(resolve => {
+            //     setTimeout(resolve, 1000)
+            // })
 
             let data = await fetch('http://localhost:3000/posts')
             if (!data.ok) {
