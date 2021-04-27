66e69e1050455166e0c85743540ccace0f7a8f29
Alan Leverenz <awleverenz@aol.com>
2021-04-27 17:12:58 -0400

    begin Firebase setup

---
+++ dogo-blog/src/firebase/config.js
@@ -0,0 +1,22 @@
+import * as keys from '../../keys'
+
+const apiKey = keys.firebaseApiKey,
+const authDomain = keys.firebaseAuthDomain,
+const databaseURL = keys.firebaseDatabaseURL,
+const projectId = keys.firebaseProjectId,
+const storageBucket = keys.storageBucket,
+const messagingSenderId = keys.firebaseMessagingSenderId,
+const appId = keys.firebaseAppId,
+
+export { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId }
+
+// const keys = {
+//     apiKey: apiKeys.keys.firebaseApiKey,
+//     authDomain: firebaseAuthDomain,
+//     databaseURL: firebaseDatabaseURL,
+//     projectId: firebaseProjectId,
+//     storageBucket: storageBucket,
+//     messagingSenderId: firebaseMessagingSenderId,
+//     appId: firebaseAppId,
+//   };
+
