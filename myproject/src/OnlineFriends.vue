c705c39ba40ce7a121e88f980a648bb366db5fa0
Alan Leverenz <awleverenz@aol.com>
2021-05-30 17:32:58 -0400

    added data to each component

--- myproject/src/OnlineFriends.vue
+++ myproject/src/OnlineFriends.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="online-friends">
         <h2>Online Friends</h2>
+            <div v-for="(friend, index) in friends" :key="index">
+                <span v-if="friend.online">{{ friend.name }}</span>
+        </div>
     </div>
 </template>
 
@@ -8,7 +11,14 @@
 export default {
     name: 'OnlineFriends',
     data() {
-
+        return {
+            friends: [
+                { name: 'Mario', online: true },
+                { name: 'Luigi', online: false },
+                { name: 'Toad', online: true },
+                { name: 'Bowser', online: false },
+            ]
+        }
     }
 }
 </script>
