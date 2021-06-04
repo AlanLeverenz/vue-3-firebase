aab56d9c9ee3d5b7f0df8a715bb62b3e1f1cc33b
Alan Leverenz <awleverenz@aol.com>
2021-06-03 20:35:58 -0400

    setup routing

---
+++ routing/src/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
+
