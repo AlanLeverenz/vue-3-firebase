189d4da5ad3dadebb0e4c521a89c8aa61e51a04a
Alan Leverenz <awleverenz@aol.com>
2021-06-24 14:47:55 -0400

    added new project - ninja-chat and setup firebase config

---
+++ ninja-chat/src/firebase/init.js
@@ -0,0 +1,17 @@
+  import firebase from 'firebase'
+  import firestore from 'firebase/firestore'
+  
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyD5k-ms7lhj3QN5sMpkbg0nF9n_UMQc63M",
+    authDomain: "udemy-ninja-chat-26ef0.firebaseapp.com",
+    projectId: "udemy-ninja-chat-26ef0",
+    storageBucket: "udemy-ninja-chat-26ef0.appspot.com",
+    messagingSenderId: "119164852618",
+    appId: "1:119164852618:web:b6cbb6cc074b98e02ad073"
+  };
+  // Initialize Firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  firebaseApp.firestore().settings({ timestampsInSnapshots: true, merge: true})
+
+  export default firebaseApp.firestore()
