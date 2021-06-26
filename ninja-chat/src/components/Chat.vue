ff5ac2fefe194689b9a653f985991f6b20cf11fc
Alan Leverenz <awleverenz@aol.com>
2021-06-25 23:15:05 -0400

    added Chat component

---
+++ ninja-chat/src/components/Chat.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="chat container">
+    <!-- 'this' is the prop in this component -->
+    <h2>Chat {{ this.name }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chat',
+  props: ['name'],
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
