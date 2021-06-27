cac7a52f3bebeb9afb835d1542eddf1b829f8cc4
Alan Leverenz <awleverenz@aol.com>
2021-06-27 16:54:50 -0400

    deployed to Firebase

--- ninja-chat/src/main.js
+++ ninja-chat/src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueChatScroll from 'vue-chat-scroll' 
+
+// registered Vue plugins
+Vue.use(VueChatScroll)
 
 Vue.config.productionTip = false
 
