16d7158f5569b879f2749da4497b2514701ee8dd
Alan Leverenz <awleverenz@aol.com>
2021-06-27 16:30:28 -0400

    formatted timestamp with moment

--- ninja-chat/src/components/Chat.vue
+++ ninja-chat/src/components/Chat.vue
@@ -23,6 +23,7 @@
 <script>
 import NewMessage from '@/components/NewMessage'
 import db from '@/firebase/init'
+import moment from 'moment'
 
 export default {
   name: 'Chat',
@@ -47,7 +48,7 @@ export default {
             id: doc.id,
             name: doc.data().name,
             content: doc.data().content,
-            timestamp: doc.data().timestamp
+            timestamp: moment(doc.data().timestamp).format('lll')
           })
         }
       })
@@ -64,8 +65,8 @@ export default {
   .chat span {
     font-size: 1.4em;
   }
-  .chat time {
+  .chat .time {
     display: block;
-    font-size: 1.2em;
+    font-size: 0.8em;
   }
 </style>
