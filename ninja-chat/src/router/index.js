e437a2925323f4fb4c8100b273aa2844999b9c33
Alan Leverenz <awleverenz@aol.com>
2021-06-28 17:45:24 -0400

    added comments to router index.js

--- ninja-chat/src/router/index.js
+++ ninja-chat/src/router/index.js
@@ -3,9 +3,11 @@ import Router from 'vue-router'
 import Welcome from '@/components/Welcome'
 import Chat from '@/components/Chat'
 
+// how to "use" a plug-in
 Vue.use(Router)
 
 export default new Router({
+  // to remove the hash sign from the URL
   mode: 'history',
   routes: [
     {
@@ -19,6 +21,7 @@ export default new Router({
       component: Chat,
       props: true,
       // 'to' is the route parameter that is being passed (name)
+      // checking for an actual params name (author)
       beforeEnter: (to, from, next) => {
         if(to.params.name){
           next()
