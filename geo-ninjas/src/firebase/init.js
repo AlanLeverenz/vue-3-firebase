db87a793d76b36f5171ebabe7c658b91721cd865
Alan Leverenz <awleverenz@aol.com>
2021-06-29 17:30:40 -0400

    added navbar

---
+++ geo-ninjas/src/firebase/init.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import firestore from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyA3-kAfAviNzVfRCe1-x6zsuFLtLsTtBq4",
+  authDomain: "udemy-geo-ninjas-eec04.firebaseapp.com",
+  projectId: "udemy-geo-ninjas-eec04",
+  storageBucket: "udemy-geo-ninjas-eec04.appspot.com",
+  messagingSenderId: "797170358871",
+  appId: "1:797170358871:web:67339c580f7bd86659a112"
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp.firestore()
