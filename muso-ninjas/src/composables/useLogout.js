24f336eaeb6c1026eabc9c95a214135c7f4f0e56
Alan Leverenz <awleverenz@aol.com>
2021-07-19 15:12:34 -0400

    added pending state

--- muso-ninjas/src/composables/useLogout.js
+++ muso-ninjas/src/composables/useLogout.js
@@ -3,10 +3,12 @@ import { projectAuth } from '../firebase/config'
 
 // refs
 const error = ref(null)
+isPending.value = false
 
 // logout function
 const logout = async () => {
   error.value = null
+  isPending.value = true
 
   try {
     await projectAuth.signOut()
@@ -14,11 +16,12 @@ const logout = async () => {
   catch(err) {
     console.log(err.message)
     error.value = err.message
+    isPending.value = false
   }
 }
 
 const useLogout = () => {
-  return { error, logout }
+  return { error, logout, isPending }
 }
 
 export default useLogout
