acdebf4e5d58633b653e67977ff7035ec8c7b04c
Alan Leverenz <awleverenz@aol.com>
2021-07-24 20:40:49 -0400

    added Navbar, button, router-links

---
+++ muso-ninjas/src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <h1>
+        <router-link :to="{ name: 'Home' }">Muso Ninjas</router-link>
+      </h1>
+      <div class="links">
+        <button>Logout</button>
+        <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
+        <router-link class="btn" :to="{ name: 'Login' }">Login</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
