c7c75493305d40a9cb368ff1b4d0b8c4d2ca5ced
Alan Leverenz <awleverenz@aol.com>
2021-08-01 10:13:16 -0400

    fixed problem with playlists

--- muso-ninjas/src/views/Home.vue
+++ muso-ninjas/src/views/Home.vue
@@ -8,18 +8,15 @@
 </template>
 
 <script>
-// get realtime data from the playlists collection
-// output 2 conditional items - error, or playlist titles
-import ListView from '@/components/ListView.vue'
-import getCollection from '@/composables/getCollection'
-
+import ListView from '../components/ListView.vue'
+import getCollection from '../composables/getCollection'
 export default {
   name: 'Home',
   components: { ListView },
   setup() {
     const { error, documents } = getCollection('playlists')
-
     return { error, documents }
   }
 }
 </script>
+
--- muso-ninjas/src/views/playlists/CreatePlaylist.vue
+++ muso-ninjas/src/views/playlists/CreatePlaylist.vue
@@ -1,43 +1,38 @@
 <template>
-  <form @submit.prevent="handleSubmit">
-    <h4>Create New Playlist</h4>
-    <input type="text" required placeholder="Playlist title" v-model="title">
-    <textarea required placeholder="Playlist description... " v-model="description" name="description"></textarea>
-    <!-- upload playlist image -->
-    <label>Upload playlist cover image</label>
-    <input type="file" @change="handleChange">
-    
-    <div class="error">{{ fileError }}</div>
+  <div>
+    <form @submit.prevent="handleSubmit">
+      <h4>Create a New Playlist</h4>
+      <input type="text" required placeholder="Playlist title" v-model="title">
+      <textarea required placeholder="Playlist description..." v-model="description"></textarea>
+      <!-- upload playlist image -->
+      <label>Upload Playlist Cover Image</label>
+      <input type="file" @change="handleChange">
+      <div class="error">{{ fileError }}</div>
 
-    <div class="error"></div>
-    <button v-if="!isPending">Create</button>
-    <button v-else disabled>Saving...</button>
-  </form>
+      <button v-if="!isPending">Create</button>
+      <button v-else disabled>Saving...</button>
+    </form>
+  </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
+import { ref } from 'vue'
 import useStorage from '@/composables/useStorage'
 import useCollection from '@/composables/useCollection'
 import getUser from '@/composables/getUser'
 import { timestamp } from '@/firebase/config'
-
 export default {
   setup() {
-
     const { filePath, url, uploadImage } = useStorage()
     const { error, addDoc } = useCollection('playlists')
     const { user } = getUser()
-
     const title = ref('')
     const description = ref('')
     const file = ref(null)
     const fileError = ref(null)
     const isPending = ref(false)
-
     const handleSubmit = async () => {
       if (file.value) {
-        // start 
         isPending.value = true
         await uploadImage(file.value)
         await addDoc({
@@ -45,27 +40,22 @@ export default {
           description: description.value,
           userId: user.value.uid,
           userName: user.value.displayName,
-          coverUrl: url.value, // public URL
-          filePath: filePath.value, // to firebase db
+          coverUrl: url.value,
+          filePath: filePath.value, // so we can delete it later
           songs: [],
-          createAt: timestamp()
+          createdAt: timestamp()
         })
-        // end
         isPending.value = false
         if (!error.value) {
           console.log('playlist added')
         }
-
       }
     }
-
-  // allowed file types
-  const types = ['image/png', 'image/jpeg']
-
+    // allowed file types
+    const types = ['image/png', 'image/jpeg']
     const handleChange = (e) => {
       let selected = e.target.files[0]
       console.log(selected)
-
       if (selected && types.includes(selected.type)) {
         file.value = selected
         fileError.value = null
@@ -74,14 +64,16 @@ export default {
         fileError.value = 'Please select an image file (png or jpg)'
       }
     }
-
-    return { title, description, handleSubmit, handleChange, fileError, isPending }
-  },
-  
+    
+    return { title, description, handleSubmit, fileError, handleChange, isPending }
+  }
 }
 </script>
 
 <style>
+  form {
+    background: white;
+  }
   input[type="file"] {
     border: 0;
     padding: 0;
@@ -91,6 +83,7 @@ export default {
     display: block;
     margin-top: 30px;
   }
-  button {margin-top: 20px}
-
+  button {
+    margin-top: 20px;
+  }
 </style>
