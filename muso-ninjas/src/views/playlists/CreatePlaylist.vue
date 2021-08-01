02ae177febefde7b3714cb2e1636df4a7c765349
Alan Leverenz <awleverenz@aol.com>
2021-08-01 10:37:38 -0400

    fetching playlist id for details page

--- muso-ninjas/src/views/playlists/CreatePlaylist.vue
+++ muso-ninjas/src/views/playlists/CreatePlaylist.vue
@@ -21,11 +21,15 @@ import useStorage from '@/composables/useStorage'
 import useCollection from '@/composables/useCollection'
 import getUser from '@/composables/getUser'
 import { timestamp } from '@/firebase/config'
+import { useRouter } from 'vue-router'
+
 export default {
   setup() {
     const { filePath, url, uploadImage } = useStorage()
     const { error, addDoc } = useCollection('playlists')
     const { user } = getUser()
+    const router = useRouter() // getting the useRouter instance
+
     const title = ref('')
     const description = ref('')
     const file = ref(null)
@@ -35,7 +39,7 @@ export default {
       if (file.value) {
         isPending.value = true
         await uploadImage(file.value)
-        await addDoc({
+        const res = await addDoc({
           title: title.value,
           description: description.value,
           userId: user.value.uid,
@@ -47,7 +51,7 @@ export default {
         })
         isPending.value = false
         if (!error.value) {
-          console.log('playlist added')
+          router.push({ name: 'PlaylistDetails', params: { id: res.id }})
         }
       }
     }
