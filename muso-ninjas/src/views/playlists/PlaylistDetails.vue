bf72c78b814c20ff46ae69eaf9e02c9cd3793497
Alan Leverenz <awleverenz@aol.com>
2021-08-04 22:19:03 -0400

    checking for user ownership

--- muso-ninjas/src/views/playlists/PlaylistDetails.vue
+++ muso-ninjas/src/views/playlists/PlaylistDetails.vue
@@ -1,14 +1,87 @@
 <template>
-  <h2>Playlist Details . ID = {{ id }}</h2>
+  <div class="error" v-if="error">{{ error }}</div>
+  <div v-if="playlist" class="playlist-details">
+
+    <!-- playlist information -->
+    <div class="playlist-info">
+      <div class="cover">
+        <img :src="playlist.coverUrl">
+      </div>
+      <h2>{{ playlist.title }}</h2>
+      <p class="username">Created by {{ playlist.userName }}</p>
+      <p class="description">{{ playlist.description }}</p>
+      <button v-if="ownership">Delete playlist</button>
+    </div>
+
+    <!-- song list -->
+    <div class="song-list">
+      <p>song list here</p>
+    </div>
+    
+  </div>
 </template>
 
 <script>
+import getDocument from '@/composables/getDocument'
+import getUser from '@/composables/getUser'
+import { computed } from '@vue/runtime-core'
+
 export default {
   // accepting id prop into the details template
-  props: ['id']
+  props: ['id'],
+  // passing props to setup method
+  setup(props) {
+    // using playlist as alias for document
+    const { error, document: playlist } = getDocument('playlists', props.id)
+    // getUser to determine ownership of playlists
+    const { user } = getUser()
+
+    const ownership = computed(() => {
+      return playlist.value && user.value && user.value.uid == playlist.value.userId
+    })
+
+    return { error, playlist, ownership }
+  }
 }
 </script>
 
 <style>
-
+  .playlist-details {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    gap: 80px;
+  }
+  .cover {
+    overflow: hidden;
+    border-radius: 20px;
+    position: relative;
+    padding: 160px;
+  }
+  .cover img {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    min-width: 100%;
+    min-height: 100%;
+    max-width: 200%;
+    max-height: 200%;
+  }
+  .playlist-info {
+    text-align: center;
+  }
+  .playlist-info h2 {
+    text-transform: capitalize;
+    font-size: 28px;
+    margin-top: 20px;
+  }
+  .playlist-info p {
+    margin-bottom: 20px;
+  }
+  .username {
+    color: #999;
+  }
+  .description {
+    text-align: left;
+  }
 </style>
