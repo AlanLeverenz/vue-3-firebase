d7e6241113f3b15c36a1e790f32562b68162d5f0
Alan Leverenz <awleverenz@aol.com>
2021-09-24 20:43:28 -0400

    updated online.vue file

---
+++ vue-cli-3/online.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="online-status">
+    <div class="online">
+      <span class="light"></span>
+      <p>A staff member is online and available</p>
+    </div>
+        <div class="offline">
+      <span class="light"></span>
+      <p>A staff member is not currently online.</p>
+    </div>
+  </div>
+</template>
